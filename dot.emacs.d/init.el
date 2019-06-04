a616325d13142b57fc873e5935b9102039c641f1
K. C. Juntunen <kcjuntunen@users.noreply.github.com>
2019-06-04 11:25:23 -0400

    Update init.el

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -1,4 +1,32 @@
-﻿(defconst kc/emacs-start-time (current-time))
+;;; init.el --- My init.el file.
+
+;; Copyright (C) 2019 K. C. Juntunen
+
+;; Author: K. C. Juntunen <juntunen.kc@gmail.com>
+;; Version: 1.0
+
+;; This file is not part of GNU Emacs.
+
+;; This is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+;; The idea is to replace the Spacemacs stuff I like, and start in less than 3 seconds.
+
+;;; Code:
+(defconst kc/emacs-start-time (current-time))
 
 (if (fboundp 'scroll-bar-mode)
     (scroll-bar-mode -1))
@@ -8,11 +36,6 @@
     (tooltip-mode -1))
 (if (fboundp 'menu-bar-mode)
     (menu-bar-mode -1))
-(setq sentence-end-double-space nil)
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq scroll-margin 0
-      scroll-conservatively 100000
-      scroll-preserve-screen-position 1)
 
 (add-to-list 'default-frame-alist '(height . 24))
 (add-to-list 'default-frame-alist '(width . 80))
@@ -26,7 +49,7 @@
 (setq package-archives '(("org" . "http://orgmode.org/elpa/")
 			 ("gnu" . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")))
-;; (package-initialize)
+(package-initialize)
 ;; (bind-key (kbd "C-x ;") 'comment-or-uncomment-region)
 
 (unless (package-installed-p 'use-package)
@@ -36,11 +59,31 @@
 (setq use-package-always-defer t)
 (setq use-package-always-ensure t)
 
+(use-package emacs
+  :custom
+  (blink-cursor-delay .2)
+  (blink-cursor-interval .2)
+  (blink-cursor-blinks 10000)
+  (scroll-step 1)
+  (sentence-end-double-space nil)
+  (scroll-margin 0)
+  (scroll-conservatively 100000)
+  (scroll-preserve-screen-position 1)
+  (show-paren-delay 0)
+  (make-backup-files nil)
+  (auto-save-default nil)
+  (whitespace-line-column 80) ;; limit line length
+  (whitespace-style '(face tabs empty trailing lines-tail))
+  :config
+  (whitespace-mode 1)
+  (show-paren-mode 1)
+  (fset 'yes-or-no-p 'y-or-n-p))
+
 (use-package which-key
   :diminish which-key
-  :init
-  (setq which-key-separator " ")
-  (setq which-key-prefix-prefix "+")
+  :custom
+  (which-key-separator " ")
+  (which-key-prefix-prefix "+")
   :config
   (which-key-mode 1))
 
@@ -84,7 +127,7 @@
     "hdc" 'describe-char
     "hdo" 'describe-symbol
     "hi"  '(info :which-key "Info")
-    "h?"  '(describe-bindings :which-key "Describe bindings") 
+    "h?"  '(describe-bindings :which-key "Describe bindings")
     "iu"  'insert-char
     "wl"  '(windmove-right :which-key "move right")
     "wh"  '(windmove-left :which-key "move left")
@@ -116,13 +159,14 @@
 (use-package spacemacs-theme
   :init
   (load-theme 'spacemacs-dark t)
-  (setq spacemacs-theme-org-agenda-height nil
-	spacemacs-theme-org-height nil))
+  :custom
+  (spacemacs-theme-org-agenda-height nil)
+  (spacemacs-theme-org-height nil))
 
 (use-package spaceline
   :demand t
-  :init
-  (setq powerline-default-separator 'arrow-fade)
+  :custom
+  (powerline-default-separator 'arrow-fade)
   :config
   (require 'spaceline-config)
   (spaceline-emacs-theme))
@@ -131,18 +175,19 @@
   :diminish helm-mode
   :init
   (require 'helm-config)
-  (setq helm-M-x-fuzzy-match t
-	helm-buffers-fuzzy-matching t
-	helm-recentf-fuzzy-match t
-	helm-locate-fuzzy-match t
-	helm-semantic-fuzzy-match t
-	helm-imenu-fuzzy-match t
-	helm-completion-in-region-fuzzy-match t
-	helm-candidate-number-list 150
-	helm-split-window-in-side-p t
-	helm-move-to-line-cycle-in-source t
-	helm-autoresize-max-height 0
-	helm-autoresize-min-height 20)
+  :custom
+  (helm-M-x-fuzzy-match t)
+  (helm-buffers-fuzzy-matching t)
+  (helm-recentf-fuzzy-match t)
+  (helm-locate-fuzzy-match t)
+  (helm-semantic-fuzzy-match t)
+  (helm-imenu-fuzzy-match t)
+  (helm-completion-in-region-fuzzy-match t)
+  (helm-candidate-number-list 150)
+  (helm-split-window-in-side-p t)
+  (helm-move-to-line-cycle-in-source t)
+  (helm-autoresize-max-height 0)
+  (helm-autoresize-min-height 20)
   :config
   (helm-mode 1)
   :general
@@ -171,6 +216,10 @@
 (use-package anzu
   :config
   (global-anzu-mode 1))
+
+(use-package flycheck
+  :hook
+   (prog-mode . flycheck-mode))
   
 ;; (use-package counsel
 ;;   :ensure t
@@ -251,40 +300,35 @@
 ;; 		      :prefix "SPC"
 ;; 		      "u" 'universal-argument-more))
 
-(setq show-paren-delay 0)
-(show-paren-mode 1)
-(setq make-backup-files nil)
-(setq auto-save-default nil)
 
 (use-package company
   :diminish company
-  :init (add-hook 'after-init-hook 'global-company-mode)
+  :init 
+  :hook
+  (prog-mode . company-mode)
+  (after-init . global-company-mode)
   :config
-  (add-hook 'prog-mode-hook 'company-mode)
   (use-package company-irony)
   (use-package company-anaconda)
-  (setq company-idle-delay              0
-        company-minimum-prefix-length   2
-        company-show-numbers            t
-        company-tooltip-limit           20
-        company-dabbrev-downcase        nil
-        company-tooltip-flip-when-above t
-        company-backends                '((company-anaconda
-                                           company-bbdb
-                                           company-elisp
-                                           company-gtags
-                                           company-irony
-                                           company-shell))))
+  :custom
+  (company-idle-delay              0)
+  (company-minimum-prefix-length   2)
+  (company-show-numbers            t)
+  (company-tooltip-limit           20)
+  (company-dabbrev-downcase        nil)
+  (company-tooltip-flip-when-above t)
+  (company-backends                '((company-anaconda
+                                      company-bbdb
+                                      company-elisp
+                                      company-gtags
+                                      company-irony
+                                      company-shell))))
 
 (use-package smartparens
   :config
   (require 'smartparens-config)
   (smartparens-global-mode 1)
   (sp-use-paredit-bindings))
-
-(whitespace-mode 1)
-(setq whitespace-line-column 80) ;; limit line length
-(setq whitespace-style '(face tabs empty trailing lines-tail))
 
 (use-package org-bullets
   :init
@@ -294,68 +338,81 @@
   :init
   (require 'evil-org)
   (require 'evil-org-agenda)
-  (add-hook 'org-mode-hook 'evil-org-mode)
+  :hook
+  (org-mode . evil-org-mode)
   :config
   (evil-org-set-key-theme '(navigation insert textobjects additional calendar))
   (evil-org-agenda-set-keys)
   :general
   (kc/mode-leader-keys
     :keymap 'org-mode-map
-    "P" 'org-set-property)
+    "P" 'org-set-property
+    "tic" 'org-table-insert-column
+    "tir" 'org-table-insert-row
+    "ti-" 'org-table-insert-hline
+    "tdc" 'org-table-delete-column)
+  (kc/leader-keys
+    :keymap 'org-agenda-mode-map
+    "TAB" '(switch-to-prev-buffer :which-key "find files")
+    "ff"  'helm-find-files
+    "fl"  'helm-locate
+    "SPC" 'helm-M-x
+    "bd"  'kill-this-buffer
+    "bb"  'helm-buffers-list)
   (kc/mode-leader-keys
     :keymap 'org-agenda-mode-map
     "l" 'org-agenda-log-mode))
-;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (use-package org
-  :config
-  (setq org-directory (if (string-equal window-system "w32")
+  :custom
+  (org-directory (if (string-equal window-system "w32")
 			  "~/../../org"
-			"~/Dropbox/org")
-	org-agenda-span 'day
-	kc/agenda-dir (concat org-directory "")
-	org-agenda-file-regexp "\\`[^.].*\\.org\\'"
-	org-use-fast-todo-selection t
-	org-treat-S-cursor-todo-selection-as-state-change nil
-	org-ellipsis "⤵"
-	kc/org-all-agenda-files (directory-files
-				 (expand-file-name kc/agenda-dir) t org-agenda-file-regexp)
-	org-refile-targets (quote ((nil :maxlevel . 1) (kc/org-all-agenda-files :maxlevel . 2)))
-	org-catch-invisible-edits 'smart
-	org-agenda-clockreport-parameter-plist '(:link t :maxlevel 4 :fileskip0 t :formula %
-                                                       :properties ("RequestNbr" "Billable" "SectionNbr" "TaskNbr"))
-	org-deadline-warning-days 45
-	org-agenda-window-setup 'current-window
-	org-agenda-skip-scheduled-if-done t
-	org-agenda-skip-deadline-if-done t
-	org-agenda-skip-timestamp-if-done t
-	org-agenda-log-mode-items '(closed clock state)
-	org-columns-default-format "%25ITEM(Task) %40Description %20Captured %10Effort(Effort){:} %10CLOCKSUM"
-	org-global-properties (quote (("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
-                                      ("STYLE_ALL" . "habit")))
-	org-todo-keywords
-	(quote ((sequence "TODO(t)" "WIP(n)" "|" "DONE(d)" "CANCELLED(c/!)")
-		(sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c/!)" "PHONE" "MEETING")))
-	org-todo-keyword-faces
-	(quote (("TODO" :foreground "red" :weight bold)
-		("WIP" :foreground "blue" :weight bold)
-		("DONE" :foreground "forest green" :weight bold)
-		("WAITING" :foreground "orange" :weight bold)
-		("HOLD" :foreground "magenta" :weight bold)
-		("CANCELLED" :foreground "forest green" :weight bold)
-		("MEETING" :foreground "forest green" :weight bold)
-		("PHONE" :foreground "forest green" :weight bold)))
-	org-todo-state-tags-triggers
-	(quote (("CANCELLED" ("ARCHIVE" . t))
-		("WAITING" ("WAITING" . t))
-		("HOLD" ("WAITING") ("HOLD" . t))
-		(done ("WAITING") ("HOLD"))
-		("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
-		("WIP" ("WAITING") ("CANCELLED") ("HOLD"))
-		("DONE" ("WAITING") ("CANCELLED") ("HOLD"))))
-	kc/refile-file (concat kc/agenda-dir "/refile.org")
-	kc/diary-file (concat org-directory "/diary.org")
-	kc/notes-file (concat org-directory "/notes.org")
-	org-capture-templates
+			"~/Dropbox/org"))
+  (org-agenda-span 'day)
+  (kc/agenda-dir (concat org-directory ""))
+  (org-agenda-file-regexp "\\`[^.].*\\.org\\'")
+  (org-use-fast-todo-selection t)
+  (org-treat-S-cursor-todo-selection-as-state-change nil)
+  (org-ellipsis "⤵")
+  (kc/org-all-agenda-files (directory-files
+			    (expand-file-name kc/agenda-dir) t org-agenda-file-regexp))
+  (org-refile-targets (quote ((nil :maxlevel . 1) (kc/org-all-agenda-files :maxlevel . 2))))
+  (org-catch-invisible-edits 'smart)
+  (org-agenda-clockreport-parameter-plist '(:link t :maxlevel 4 :fileskip0 t :formula %
+                                                  :properties ("RequestNbr" "Billable" "SectionNbr" "TaskNbr")))
+  (org-deadline-warning-days 45)
+  (org-agenda-window-setup 'current-window)
+  (org-agenda-skip-scheduled-if-done t)
+  (org-agenda-skip-deadline-if-done t)
+  (org-agenda-skip-timestamp-if-done t)
+  (org-agenda-log-mode-items '(closed clock state))
+  (org-columns-default-format "%25ITEM(Task) %40Description %20Captured %10Effort(Effort){:} %10CLOCKSUM")
+  (org-global-properties (quote (("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
+                                      ("STYLE_ALL" . "habit"))))
+  (org-todo-keywords
+   (quote ((sequence "TODO(t)" "WIP(n)" "|" "DONE(d)" "CANCELLED(c/!)")
+	   (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c/!)" "PHONE" "MEETING"))))
+  (org-todo-keyword-faces
+   (quote (("TODO" :foreground "red" :weight bold)
+	   ("WIP" :foreground "blue" :weight bold)
+	   ("DONE" :foreground "forest green" :weight bold)
+	   ("WAITING" :foreground "orange" :weight bold)
+	   ("HOLD" :foreground "magenta" :weight bold)
+	   ("CANCELLED" :foreground "forest green" :weight bold)
+	   ("MEETING" :foreground "forest green" :weight bold)
+	   ("PHONE" :foreground "forest green" :weight bold))))
+  (org-todo-state-tags-triggers
+   (quote (("CANCELLED" ("ARCHIVE" . t))
+	   ("WAITING" ("WAITING" . t))
+	   ("HOLD" ("WAITING") ("HOLD" . t))
+	   (done ("WAITING") ("HOLD"))
+	   ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+	   ("WIP" ("WAITING") ("CANCELLED") ("HOLD"))
+	   ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
+  (kc/refile-file (concat kc/agenda-dir "/refile.org"))
+  (kc/diary-file (concat org-directory "/diary.org"))
+  (kc/notes-file (concat org-directory "/notes.org"))
+  (org-capture-templates
 	'(("t" "todo" entry
            (file kc/refile-file)
            "* TODO %?
@@ -396,8 +453,28 @@
     :keymap 'org-mode-map
     "tic" 'org-table-insert-column
     "tir" 'org-table-insert-row))
-;; (defvar kc/agenda-files-file ".agenda_work_files")
-;; (setq org-agenda-files (concat kc/agenda-dir "/" kc/agenda-files-file))
+
+(defun kc/copy-query-notes ()
+  "Copy a query string to the clipboard for the `notes' table for the last hour."
+  (interactive)
+  (w32-set-clipboard-data
+   (format "SELECT * FROM notes WHERE NoteDateTime > '%s' AND Tag1 LIKE '%%ATHOS%%'"
+	   (format-time-string "%Y-%m-%d %H:%M:%S"
+			       (time-subtract (current-time)
+					      (seconds-to-time (* 60 60 30))) "wall"))))
+
+(defun kc/copy-query-incidents ()
+  "Copy a query string to the clipboard for the `incidents' table for the last hour."
+  (interactive)
+  (w32-set-clipboard-data
+   (format "SELECT * FROM incidents WHERE DateAdded > '%s' AND Resolved <> 'Y'"
+	   (format-time-string "%Y-%m-%d %H:%M:%S"
+			       (time-subtract (current-time)
+					      (seconds-to-time (* 60 60 30))) "wall"))))
+(kc/leader-keys
+  "cqn" 'kc/copy-query-notes
+  "cqi" 'kc/copy-query-incidents)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -405,15 +482,22 @@
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
  '(org-agenda-files
-   '("~/Dropbox/org/work.org" "~/Dropbox/org/theisen.org" "~/Dropbox/org/plantation.org" "~/Dropbox/org/palko.org" "~/Dropbox/org/orsheln.org" "~/Dropbox/org/notes.org" "~/Dropbox/org/north40.org" "~/Dropbox/org/mfwestern.org" "~/Dropbox/org/kelsan.org" "~/Dropbox/org/jci.org" "~/Dropbox/org/clarion.org" "~/Dropbox/org/berne.org"))
+   (quote
+    ("c:/Users/K.C.Juntunen/OneDrive/org/superior.org" "~/../../OneDrive/org/work.org" "~/../../OneDrive/org/theisen.org" "~/../../OneDrive/org/plantation.org" "~/../../OneDrive/org/palko.org" "~/../../OneDrive/org/orsheln.org" "~/../../OneDrive/org/notes.org" "~/../../OneDrive/org/north40.org" "~/../../OneDrive/org/mfwestern.org" "~/../../OneDrive/org/kelsan.org" "~/../../OneDrive/org/jci.org" "~/../../OneDrive/org/clarion.org" "~/../../OneDrive/org/berne.org")))
  '(package-selected-packages
-   '(company-jedi company-anaconda pretty-symbols magit use-package)))
+   (quote
+    (csharp-mode csv-mode company-jedi company-anaconda pretty-symbols magit use-package)))
+ '(scroll-conservatively 100000)
+ '(scroll-margin 0)
+ '(scroll-preserve-screen-position 1)
+ '(scroll-step 1)
+ '(sentence-end-double-space nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(cursor ((t (:background "red")))))
 
 (load-file "~/.personal.el")
 (defconst kc/after-init (current-time))
@@ -428,3 +512,6 @@
                  (message "Loading %s...done (%.3fs) [after-init]"
                           ,load-file-name elapsed)))
 	    t))
+
+(provide 'init)
+;;; init.el ends here
