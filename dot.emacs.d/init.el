b3b4889468f1164a5b4eeb21ef07f8877144183a
K. C. Juntunen <juntunen.kc@gmail.com>
2019-07-27 22:17:44 -0400

    It's just confusing when the line is broken.

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -140,7 +140,13 @@
 (use-package evil
   :config
   (evil-mode 1)
-  (setq-default evil-respect-visual-line-mode t))
+  (setq-default evil-respect-visual-line-mode t)
+  (define-key evil-normal-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+  (define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+  (define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+  (define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+  ;; Make horizontal movement cross lines
+  (setq-default evil-cross-lines t))
 
 (use-package evil-collection
   :defines
