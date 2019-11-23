2515ec0c3a9ab28278ead1c2a4cf150bc5e52bac
K. C. Juntunen <juntunen.kc@gmail.com>
2019-11-23 11:23:46 -0500

    Don't hang on close.

--- dot.emacs.d/lin-init.el
+++ dot.emacs.d/lin-init.el
@@ -1,5 +1,7 @@
 ;; lin-init.el --- Linux-specific configuration. -*- lexical-binding: t -*-
 (message "Using Linux! 😃")
 
+(setq x-select-enable-clipboard-manager nil)
+
 (provide 'lin-init)
 ;; lin-init ends here
